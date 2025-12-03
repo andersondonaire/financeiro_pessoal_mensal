6f7be7414d0db5e6d82a126183211f3ff7390802
Anderson Donaire <andersondonaire@hotmail.com>
2025-12-03 09:45:10 -0300

    fix: correct database.php case sensitivity for Linux

--- src/Models/Ciclo.php
+++ src/Models/Ciclo.php
@@ -4,7 +4,7 @@
  */
 
 require_once __DIR__ . '/../../config/config.php';
-require_once __DIR__ . '/../../config/Database.php';
+require_once __DIR__ . '/../../config/database.php';
 
 class Ciclo
 {
