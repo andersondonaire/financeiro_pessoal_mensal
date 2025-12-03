0451c89b2a069c6b34b949b098ea0b13c3cc9816
Anderson Donaire <andersondonaire@hotmail.com>
2025-12-03 09:48:25 -0300

    fix: force TCP/IP connection instead of Unix socket

--- config/database.php
+++ config/database.php
@@ -12,7 +12,13 @@ class Database
     private function __construct()
     {
         try {
-            $dsn = 'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=' . DB_CHARSET;
+            // Forçar conexão TCP/IP para evitar problemas com socket Unix
+            $host = DB_HOST;
+            if ($host === 'localhost') {
+                $host = '127.0.0.1';
+            }
+            
+            $dsn = 'mysql:host=' . $host . ';dbname=' . DB_NAME . ';charset=' . DB_CHARSET;
             
             $opcoes = [
                 PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
